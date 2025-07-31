5f3bb55c0f56d3d810f1a01159621efd8f354e4c
VloRyan <florian685@gmail.com>
2025-07-31 15:51:56 +0200

    fix: use initOpts before defaults

--- hooks/UseQueryOpts.ts
+++ hooks/UseQueryOpts.ts
@@ -3,12 +3,10 @@ import { useFilter } from "./UseFilter.ts";
 import { FetchOpts } from "../jsonapi/JsonApi.ts";
 
 export const useQueryOpts = (init?: FetchOpts) => {
-  const page = usePage();
-  const filter = useFilter();
   const urlParams = new URLSearchParams(window.location.search);
   return {
-    page: page ?? init?.page,
-    filter: filter ?? init?.filter,
+    page: init?.page ? init.page : usePage(),
+    filter: init?.filter ? init.filter : useFilter(),
     sort: urlParams.get("sort") ?? init?.sort,
     includes: init?.includes,
   } as FetchOpts;
